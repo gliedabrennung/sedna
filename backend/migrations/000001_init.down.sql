4623b13b49523fb6aba8dccc7f383b8dd9825be4
Bekzat Saparbekov <bekzatsaparbekov070@gmail.com>
2026-05-17 20:17:36 +0500

    many changes

---
+++ backend/migrations/000001_init.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS messages;
+DROP TRIGGER IF EXISTS trg_users_updated_at ON users;
+DROP FUNCTION IF EXISTS set_updated_at();
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS user_id_seq;
